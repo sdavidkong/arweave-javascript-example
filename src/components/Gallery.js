db017ffc716c7ad717c332320fa4b229d8e9ad56
S David Kong <SDAVIDKONG@gmail.com>
2023-01-22 13:09:53 -0800

    added Gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,15 @@
+{
+    transactions(
+      owners: ["5VPys2doO8c-eX2wKu07u9Fh2d_K7PtZ6pThV_K1I6s"], 
+      tags: {
+              name: "Content-Type",
+              values: ["image/png"]
+          }) {
+      edges {
+        node {
+          id
+         
+        }
+      }
+    }
+  }
